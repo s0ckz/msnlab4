be8863a90099bc253afaad14d8e9034efe3c95cf
diegotavarescg <diegotavarescg@e2a02be8-bc49-11de-b431-8336cfaced44>
2009-11-27 00:27:07 +0000

    Mudança pequena na interface.

--- trunk/src/br/edu/ufcg/msn/interpolacao/spline3d/MultivariateRealInterpolator.java
+++ trunk/src/br/edu/ufcg/msn/interpolacao/spline3d/MultivariateRealInterpolator.java
@@ -17,7 +17,8 @@ public interface MultivariateRealInterpolator {
 	/**
 	 * Computes an interpolating function for the data set. 
 	 * 
-	 * @param xval the arguments for the interpolation points. xval[i][0] is the first component of interpolation point i, xval[i][1] is the second component, and so on until xval[i][d-1], the last component of that interpolation point (where d is thus the dimension of the space).
+	 * @param x1 x1 values in tabulated function f(x1,x2)
+	 * @param x2 x2 values in tabulated function f(x1,x2)
 	 * @param yval the values for the interpolation points 
 	 * @return a function which interpolates the data set
 	 * @throws MathException if arguments violate assumptions made by the interpolation algorithm or some dimension mismatch occurs 
@@ -25,5 +26,5 @@ public interface MultivariateRealInterpolator {
 	 * 
 	 * @see http://commons.apache.org/math/apidocs/org/apache/commons/math/analysis/interpolation/MultivariateRealInterpolator.html#interpolate(double[][],%20double[])
 	 */
-	MultivariateRealFunction interpolate(double[][] xval, double[] yval) throws MathException, IllegalArgumentException;
+	MultivariateRealFunction interpolate(double[] x1, double[] x2, double[][] yval) throws MathException, IllegalArgumentException;
 }
